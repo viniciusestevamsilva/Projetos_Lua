2964c14fd14ee59ecbeded3930c567e8ce227605
viniciusestevamsilva <viniciusestevamsilvajf@gmail.com>
2025-01-28 16:50:21 -0300

    Par ou Impar

---
+++ projetos/par_ou_impar.lua
@@ -0,0 +1,12 @@
+print("Digite um número:")
+print("-"*70)
+
+num = tonumber(io.read())
+
+if num % 2 == 0 then
+    print("O número é par.")
+else
+    print("O número é ímpar.")
+end
+
+print("-"*70)
