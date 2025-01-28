73ce396843e653ceb5c8660c2f613eb33302f767
viniciusestevamsilva <viniciusestevamsilvajf@gmail.com>
2025-01-28 16:51:56 -0300

    Tabuada de um unico numero

---
+++ projetos/tabuada.lua
@@ -0,0 +1,13 @@
+print("Digite um número para ver sua tabuada:")
+num = tonumber(io.read())
+
+print("-"*70)
+
+for i = 1, 10 do
+    print(num .. " x " .. i .. " = " .. num * i)
+end
+
+print("Digite um número para ver sua tabuada:")
+num = tonumber(io.read())
+
+print("-"*70)
