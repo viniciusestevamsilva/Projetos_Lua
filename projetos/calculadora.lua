e7ea2db34b80c364b581f87bee712aec65ce60ec
viniciusestevamsilva <viniciusestevamsilvajf@gmail.com>
2025-01-28 16:48:35 -0300

    Calculadora bem simples

---
+++ projetos/calculadora.lua
@@ -0,0 +1,10 @@
+print("-"*70)
+print("Digite o primeiro número:")
+num1 = tonumber(io.read())
+
+print("Digite o segundo número:")
+num2 = tonumber(io.read())
+soma = num1 + num2
+
+print("A soma é: " .. soma)
+print("-"*70)
